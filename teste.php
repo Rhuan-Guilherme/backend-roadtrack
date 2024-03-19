b4bd569662633dcba5660071d859eaaeec1bfefa
Rhuan Guilherme <rhuan.g.silva@stf.jus.br>
2024-03-19 18:14:47 -0300

    Teste com Authorization

--- teste.php
+++ teste.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Methods: POST"); // Permitir apenas métodos POST
-header("Access-Control-Allow-Headers: Content-Type"); // Permitir apenas o cabeçalho Content-Type
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 include('./ValidarToken.php');
 
@@ -18,4 +18,24 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo $dadosToken;
   }
 
+}
+
+if($_SERVER["REQUEST_METHOD"] === "GET"){
+  echo "Cabeçalho Authorization: " . $_SERVER["HTTP_AUTHORIZATION"] . "\n";
+  $token = $_SERVER["HTTP_AUTHORIZATION"];
+  var_dump($token);
+
+  if($token){
+    $valida = validaToken($token);
+
+    if(!$valida){
+      echo 'Token Inválido!';
+  } else {
+      $dadosToken = returnData($token);
+      echo 'Token Válido!';
+      echo $dadosToken;
+  }
+  } else {
+    echo 'Token não fornecido!';
+  }
 }
